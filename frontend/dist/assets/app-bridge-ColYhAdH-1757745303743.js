91f678cce5a622f6a43cc047dbf0cf2274710da2
Christo Stefanov <christostefanov@Christos-MacBook-Pro.local>
2025-09-13 09:35:57 +0300

    Navigation Fix after partners install fix

--- frontend/dist/assets/app-bridge-ColYhAdH-1757745303743.js
+++ frontend/dist/assets/app-bridge-ColYhAdH-1757745303743.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-tAaa2TlE-1757710021475.js";var f={exports:{}},n={};/**
+import{r as i}from"./react-vendor-tAaa2TlE-1757745303743.js";var f={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
