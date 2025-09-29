26d0fed0515a8206823f20643491bdb14bc88c98
Christo Stefanov <christostefanov@Christos-MacBook-Pro.local>
2025-09-29 15:24:19 +0300

    Build frontend with reordered AI Discovery features

--- frontend/dist/assets/app-bridge-DNSAswl--1759148651390.js
+++ frontend/dist/assets/app-bridge-DNSAswl--1759148651390.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-tAaa2TlE-1759136429714.js";var f={exports:{}},n={};/**
+import{r as i}from"./react-vendor-tAaa2TlE-1759148651390.js";var f={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
