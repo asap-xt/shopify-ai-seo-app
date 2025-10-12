1e3917a4fbcb2b5269d3079208f64ce4f1e60524
Christo Stefanov <christostefanov@Christos-MacBook-Pro.local>
2025-10-12 21:15:44 +0300

    build: Rebuild frontend with Collections modal fixes

--- frontend/dist/assets/app-bridge-lQpXPbhX-1760292930035.js
+++ frontend/dist/assets/app-bridge-lQpXPbhX-1760292930035.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-tAaa2TlE-1760289497033.js";var f={exports:{}},n={};/**
+import{r as i}from"./react-vendor-tAaa2TlE-1760292930035.js";var f={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
