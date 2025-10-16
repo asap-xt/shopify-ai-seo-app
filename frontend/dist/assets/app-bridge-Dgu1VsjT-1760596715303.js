9ca8879e51aeaf9b3bcc1e579c99a941ba0dac34
Christo Stefanov <christostefanov@Christos-MacBook-Pro.local>
2025-10-16 09:38:46 +0300

    Add collections/update webhook registration

--- frontend/dist/assets/app-bridge-Dgu1VsjT-1760596715303.js
+++ frontend/dist/assets/app-bridge-Dgu1VsjT-1760596715303.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-tAaa2TlE-1760595989448.js";var f={exports:{}},n={};/**
+import{r as i}from"./react-vendor-tAaa2TlE-1760596715303.js";var f={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
