f53625e8307fdc41af885ab1a7744c80ad8df6b1
Christo Stefanov <christostefanov@Christos-MacBook-Pro.local>
2025-10-16 17:04:36 +0300

    Rebuild frontend with Uninstall Preparation feature

--- frontend/dist/assets/app-bridge-Bq3vqj6n-1760623456587.js
+++ frontend/dist/assets/app-bridge-Bq3vqj6n-1760623456587.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-tAaa2TlE-1760620994489.js";var f={exports:{}},n={};/**
+import{r as i}from"./react-vendor-tAaa2TlE-1760623456587.js";var f={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
