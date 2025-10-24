5b654a115da52365334edb87e8ee687fac6d4d2d
Christo Stefanov <christostefanov@Christos-MacBook-Pro.local>
2025-10-24 12:22:07 +0300

    build(frontend): rebuild with store metadata fixes

--- frontend/dist/assets/app-bridge-Cc_mvh1R-1761297693626.js
+++ frontend/dist/assets/app-bridge-Cc_mvh1R-1761297693626.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-tAaa2TlE-1761161410222.js";var f={exports:{}},n={};/**
+import{r as i}from"./react-vendor-tAaa2TlE-1761297693626.js";var f={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
